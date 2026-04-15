433939f0a5f271459a54cffab4e41e07d2b2b8e1
Kazeem Jimoh <holaryinka5050@gmail.com>
2026-04-15 13:07:08 +0100

    update

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://44.195.83.48:8080',
+    baseURL:'http://44.193.39.54:8080',
     headers: {
         'Content-Type': 'application/json',
     },
